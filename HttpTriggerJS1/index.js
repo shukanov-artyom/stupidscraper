d1e349388aef3f4375ed699dbe2a7706326b7eb4
Artyom Shukanov <shukanov.artyom@gmail.com>
2018-02-07 09:40:14 +0300

    filter out styles

--- HttpTriggerJS1/index.js
+++ HttpTriggerJS1/index.js
@@ -56,5 +56,6 @@ function cleanse(textData){
     return textData
         .replace(/\s+/gm, ' ') // replace multiple space characters with one space
         .replace(/(\r\n|\n|\r)+/gm, ' ') // remove carriage retrun / newline
-        .replace(/(<\S+>\S*<\/\S+>)+/gm, ' '); // remove html tags included in text
+        .replace(/(<\S+>\S*<\/\S+>)+/gm, ' ') // remove html tags included in text
+        .replace(/\S+\s*{.+}/gm, ''); // attempt to throw away all JSON (like styles)
 }
